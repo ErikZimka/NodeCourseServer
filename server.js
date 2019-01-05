32c8a71db11ade91cf4cf4faf5bddfc8da33d1c7
Zimka Erik <zimka.erik@gmail.com>
2019-01-05 11:09:46 +0100

    Add projects page

--- server.js
+++ server.js
@@ -46,6 +46,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
+
 app.listen(port, () => {
   console.log(`Server on port: ${port}`);
 });
